054ce5c157b47209f47986d42a3cb8863c11f2b7
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2024-03-09 00:04:24 +0330

    template

---
+++ Assist/header.php
@@ -0,0 +1,41 @@
+
+<header>
+    <div class="container-fluid position-fixed z-2 bg-light">
+      <div class="row flex-row justify-content-lg-center align-items-lg-center">
+        <div class="col-2 col-lg-2 order-2 order-lg-0">
+          <h1 class="fw-bold">
+            MOEIN
+          </h1>
+        </div>
+        <div class="col-10 order-1 order-lg-0 col-lg-8">
+          <nav class="navbar navbar-expand-lg">
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-lg-center align-items-lg-center" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">contact us</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div class="d-none d-lg-block col-lg-2">
+          <ul class="d-flex flex-row gap-3  ">
+            <li class="list-unstyled "><a class="text-dark" href="#"><i class="bi bi-linkedin"></i></a></li>
+            <li class="list-unstyled "><a class="text-dark" href="#"><i class="bi bi-instagram"></i></a></li>
+            <li class="list-unstyled "><a class="text-dark" href="#"><i class="bi bi-twitter-x"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </header>
--- index.php
+++ index.php
@@ -1,33 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Css/bootstrap.min.css">
-    <title>Portfolio</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./Css/bootstrap.min.css">
+  <link rel="stylesheet" href="./Fonts/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="./Css/index.css">
+  <title>Portfolio</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <?php
+  require_once("./Assist/header.php");
+  ?>
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
+    <div class="row"id="slider" >
+      <div class="col-12 position-relative" >
+        <div class="position-absolute">
+          <h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, ipsum
+          </h2>
+          <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, ipsum
+          </p>
+        </div>
+        
+      </div>
     </div>
   </div>
-</nav>
+  <div class="container-fluid">
+    <div class="row justify-content-center align-items-center">
+      <h1 class="text-center">Contact us</h1>
+      <div class="col-10 col-md-8 col-lg-6  ">
+        <form action="" method="post">
+          <div class="mb-3">
+            <label  class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" >
+          </div>
+          <div class="mb-3">
+            <label  class="form-label">Description</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button class="btn btn-success">submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 <script src="./Script/bootstrap.bundle.min.js"></script>
+
 </html>
