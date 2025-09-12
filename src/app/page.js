2a03007b18a150d13a3225773ab41841b4471d11
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-09-12 17:15:37 +0330

    Add animation to tiltles

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,50 @@
 import AboutMe from "./components/About/aboutMe";
+import Career from "./components/Career/Career";
 import ContactUs from "./components/Contact/form";
 import Head from "./components/Head/Head";
 import Projects from "./components/Projects/project";
+import BlurText from "./components/ui/BlurText";
 
 export default function App() {
   return (
     <>
-    <Head/>
-    <AboutMe/>
-    <Projects/>
-    <ContactUs/>
+      <Head />
+      <BlurText
+        text="About me"
+        delay={150}
+        animateBy="letters"
+        direction="top"
+        
+        className="text-2xl mb-8"
+      />
+      <AboutMe />
+        <BlurText
+        text="My career"
+        delay={150}
+        animateBy="letters"
+        direction="top"
+        
+        className="text-2xl mb-8"
+      />
+      <Career/>
+      <BlurText
+        text="My projects"
+        delay={150}
+        animateBy="letters"
+        direction="top"
+        
+        className="text-2xl mb-8"
+      />
+      <Projects />
+      <BlurText
+        text="Get in touch"
+        delay={150}
+        animateBy="letters"
+        direction="top"
+        
+        className="text-2xl mb-8"
+      />
+      <ContactUs />
     </>
   );
 }
