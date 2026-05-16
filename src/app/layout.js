d6b321ed8aa8efb16052f8b4ee94cfbea552fad5
mohamad-hk <hm.karimi.23179@gmail.com>
2026-05-16 15:15:07 +0330

    fix: update metadata title for clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,25 +4,19 @@ import Header from "./container/header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Mohammad hossein karimi | Developer Portfolio",
+  title: "Mohammad hossein karimi | full stack web developer",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico"  sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      </head>
       <body className="relative text-white">
-        <Background/>
-        <Header/>
+        <Background />
+        <Header />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
