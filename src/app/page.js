49655019e90cbc860fa570179bd1e80351d8fb4e
mohamad-hk <hm.karimi.23179@gmail.com>
2026-05-18 16:59:46 +0330

    feat: redirect to English route

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,5 @@
-import Career from "./components/Career/Career";
-import ContactUs from "./components/Contact/form";
-import Head from "./components/Head/Head";
-import Projects from "./components/Projects/project";
-import Skills from "./components/Skills/Skills";
-import BlurText from "./components/ui/BlurText";
+import { redirect } from "next/navigation";
 
-export default async function App() {
-  const res = await fetch(`${process.env.API_BASE_URL}/api/projects`, {
-    cache: "no-store",
-  });
-  const project_data = await res.json();
-  return (
-    <main className=" flex flex-col gap-6">
-      <Head />
-
-      <BlurText
-        text="Technical Stack"
-        delay={150}
-        animateBy="letters"
-        direction="top"
-        className="text-2xl mb-8"
-        id="stack"
-      />
-      <Skills />
-
-      <BlurText
-        text="My career"
-        delay={150}
-        animateBy="letters"
-        direction="top"
-        className="  text-2xl mb-8"
-        id="experience"
-      />
-      <Career />
-      <BlurText
-        text="My projects"
-        delay={150}
-        animateBy="letters"
-        direction="top"
-        className="text-2xl mb-8"
-        id="projects"
-      />
-      <Projects project_data={project_data} />
-      <BlurText
-        text="Let's build something"
-        delay={150}
-        animateBy="letters"
-        direction="top"
-        className="text-2xl mb-8"
-        id="contact"
-      />
-      <ContactUs />
-    </main>
-  );
+export default function RootPage() {
+  redirect("/en");
 }
