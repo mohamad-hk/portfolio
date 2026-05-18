baad5b900b2ccd79fea9a99a4d8b222d957c4ab8
mohamad-hk <hm.karimi.23179@gmail.com>
2026-05-18 16:57:53 +0330

    feat: enhance API routes for bilingual support

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -1,11 +1,39 @@
 import pool from "@/app/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req) {
   try {
-    const result = await pool.query(
-      "SELECT * FROM tbl_projects"
-    );
+    const { searchParams } = new URL(req.url);
+
+    const locale = searchParams.get("locale") || "en";
+    const isFa = locale === "fa";
+    const result = await pool.query(`
+      SELECT
+        project_id,
+        project_name,
+        project_demo,
+        project_link,
+        project_technologies,
+
+        ${
+          isFa
+            ? `
+          COALESCE(project_description_fa, project_description) AS project_description,
+          COALESCE(project_problem_fa, project_problem) AS project_problem,
+          COALESCE(project_solution_fa, project_solution) AS project_solution
+        `
+            : `
+          project_description,
+          project_problem,
+          project_solution
+        `
+        }
+
+      FROM tbl_projects
+
+      ORDER BY project_id DESC
+    `);
+
     return NextResponse.json({ data: result.rows }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
