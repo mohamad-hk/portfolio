ae1a6af494876f8deb1bad31374e3bacacaabf16
mohamad-hk <hm.karimi.23179@gmail.com>
2026-05-19 19:09:05 +0330

    feat: implement profile card

--- src/app/components/Head/ProfileImage/pfp.js
+++ src/app/components/Head/ProfileImage/pfp.js
@@ -1,9 +1,20 @@
-import Image from "next/image"
+import TiltedCard from "../../ui/ProfileCard";
 
-const ProfileImage=()=>{
-
-    return(
-        <Image src="/images/me.jpg" className="mx-auto" width={400} height={400} alt="image not found"/>
-    )
-}
-export default ProfileImage
+const ProfileImage = () => {
+  return (
+    <TiltedCard
+      imageSrc="/images/personal-image.webp"
+      altText="Mohammad hossein karimi"
+      containerHeight="300px"
+      containerWidth="300px"
+      imageHeight="300px"
+      imageWidth="300px"
+      rotateAmplitude={3}
+      scaleOnHover={1.05}
+      showMobileWarning={false}
+      showTooltip={false}
+      displayOverlayContent={false}
+    />
+  );
+};
+export default ProfileImage;
