65ccefec5345215fcee0ba5ac77a245a151a8cb3
mohamad-hk <hm.karimi.23179@gmail.com>
2026-06-03 22:31:25 +0330

    fix: add Google Tag Manager script

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -9,6 +9,7 @@ import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { enFont, faFont } from "@/app/fonts";
 import DotGrid from "../components/ui/Background";
+import Script from "next/script";
 
 export async function generateMetadata({ params }) {
   const { locale } = await params;
@@ -50,11 +51,28 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html lang={locale} dir={locale === "fa" ? "rtl" : "ltr"} className={`${enFont.variable} ${faFont.variable}`}>
+      <head>
+        <Script>
+          {`  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GCJRM5T');`}
+        </Script>
+      </head>
       <body
         className={`relative min-h-screen overflow-x-hidden text-white bg-[#001219] ${
           locale === "en" ? "[font-family:var(--font-en)]" : "[font-family:var(--font-fa)]"
         }`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GCJRM5T"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <div className="fixed inset-0 z-0 pointer-events-none opacity-50">
             <DotGrid
